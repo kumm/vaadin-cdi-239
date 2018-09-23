a77f075055dfce866eed316661aa769c056f77ff
kumm <kumm@webstar.hu>
2018-09-23 10:51:01 +0200

    Use CDINavigator with vaadin-cdi 3.0

--- src/main/java/at/ppichl/test/vaadincdi/NavigationService.java
+++ src/main/java/at/ppichl/test/vaadincdi/NavigationService.java
@@ -1,20 +1,12 @@
 package at.ppichl.test.vaadincdi;
 
-import com.vaadin.cdi.UIScoped;
+import com.vaadin.cdi.CDINavigator;
+import com.vaadin.cdi.NormalUIScoped;
 import com.vaadin.cdi.internal.Conventions;
-import com.vaadin.navigator.Navigator;
 import com.vaadin.navigator.View;
-import com.vaadin.navigator.ViewDisplay;
-import com.vaadin.navigator.ViewProvider;
-import com.vaadin.ui.UI;
 
-@UIScoped
-public class NavigationService extends Navigator  {
-
-    public void init(UI ui, ViewProvider viewProvider, ViewDisplay display) {
-        init(ui, new UriFragmentManager(ui.getPage()),display);
-        addProvider(viewProvider);
-    }
+@NormalUIScoped
+public class NavigationService extends CDINavigator {
 
     public void navigateTo(Class<? extends View> targetView) {
         String viewId = getViewId(targetView);
--- src/main/java/at/ppichl/test/vaadincdi/StoreUI.java
+++ src/main/java/at/ppichl/test/vaadincdi/StoreUI.java
@@ -3,18 +3,14 @@ package at.ppichl.test.vaadincdi;
 import at.ppichl.test.vaadincdi.views.MainView;
 import at.ppichl.test.vaadincdi.views.OrderView;
 import com.vaadin.cdi.CDIUI;
-import com.vaadin.cdi.CDIViewProvider;
+import com.vaadin.navigator.ViewDisplay;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.server.VaadinSession;
 import com.vaadin.ui.UI;
 
 import javax.inject.Inject;
 
 @CDIUI("ui")
 public class StoreUI extends UI {
-
-    @Inject
-    CDIViewProvider cdiViewProvider;
 
     @Inject
     NavigationService navigationService;
@@ -25,7 +21,7 @@ public class StoreUI extends UI {
     @Override
     protected void init(VaadinRequest vaadinRequest) {
         setContent(mainView);
-        navigationService.init(this, cdiViewProvider, mainView);
+        navigationService.init(this, (ViewDisplay) mainView);
         navigationService.navigateTo(OrderView.class);
     }
 }
